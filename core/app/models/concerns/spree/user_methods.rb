fd2532ca8d38463894007ca0119857c913d44772
Tanmay Sinha <tanmay3011@gmail.com>
2016-01-26 19:36:34 +0530

    Add dependent destroy option to role_users association

--- core/app/models/concerns/spree/user_methods.rb
+++ core/app/models/concerns/spree/user_methods.rb
@@ -8,7 +8,7 @@ module Spree
     include Spree::RansackableAttributes
 
     included do
-      has_many :role_users, class_name: 'Spree::RoleUser', foreign_key: :user_id
+      has_many :role_users, class_name: 'Spree::RoleUser', foreign_key: :user_id, dependent: :destroy
       has_many :spree_roles, through: :role_users, class_name: 'Spree::Role', source: :role
 
       has_many :promotion_rule_users, class_name: 'Spree::PromotionRuleUser'
