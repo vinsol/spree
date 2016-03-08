055961b6c22242aa513e9c713ae5054ec30d1d8d
jaspreet21anand <jaspreet21anand@gmail.com>
2016-03-08 17:08:49 +0530

    assign default store when creating order from admin/api
    No default store were assigned when we created order from admin end or from api
    
    this rose exception in emails where url creation helpers based on stores where used
    
    this issue was raised in spree_digital where we used to provide links for downloading in emails https://github.com/spree-contrib/spree_digital/issues/92

--- backend/app/controllers/spree/admin/orders_controller.rb
+++ backend/app/controllers/spree/admin/orders_controller.rb
@@ -128,7 +128,8 @@ module Spree
       private
         def order_params
           params[:created_by_id] = try_spree_current_user.try(:id)
-          params.permit(:created_by_id, :user_id)
+          params[:store_id] ||= current_store.id
+          params.permit(:created_by_id, :user_id, :store_id)
         end
 
         def load_order
