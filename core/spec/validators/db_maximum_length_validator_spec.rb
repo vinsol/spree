bd7efdbef1c9d17a9523d04b9f4dc5d6d2928ded
Priyank Gupta <priyank.gupta@vinsol.com>
2016-06-29 17:56:05 +0530

    [Rails 5] use shoulda matchers to spec DbMaximumLengthValidator

--- core/spec/validators/db_maximum_length_validator_spec.rb
+++ core/spec/validators/db_maximum_length_validator_spec.rb
@@ -7,21 +7,18 @@ end
 
 describe DbMaximumLengthValidator, type: :model do
   let(:limit_for_slug) { Spree::Product.columns_hash['slug'].limit.to_i }
-  let(:product) { Spree::Product.new }
-  let(:slug) { 'x' * (limit_for_slug + 1) }
+  let(:product) { create(:product) }
+
+  subject { product }
 
   context 'when limit present' do
     before do
       sql_type_metadata = ActiveRecord::ConnectionAdapters::SqlTypeMetadata.new(sql_type: 'varchar', type: :string, limit: 100)
       adapter_column = ActiveRecord::ConnectionAdapters::Column.new('slug', nil, sql_type_metadata, true, 'spree_products')
       Spree::Product.columns_hash['slug'] = adapter_column
-      product.slug = slug
     end
 
-    it 'validates DB maximum length' do
-      product.valid?
-      expect(product.errors[:slug]).to include(I18n.t("errors.messages.too_long", count: limit_for_slug))
-    end
+    it { should validate_length_of(:slug).is_at_most(limit_for_slug) }
   end
 
   context 'when limit not present' do
@@ -29,12 +26,8 @@ describe DbMaximumLengthValidator, type: :model do
       sql_type_metadata = ActiveRecord::ConnectionAdapters::SqlTypeMetadata.new(sql_type: 'varchar', type: :string, limit: nil)
       adapter_column = ActiveRecord::ConnectionAdapters::Column.new('slug', nil, sql_type_metadata, true, 'spree_products')
       Spree::Product.columns_hash['slug'] = adapter_column
-      product.slug = slug
     end
 
-    it 'do not validate DB maximum length' do
-      product.valid?
-      expect(product.errors[:slug]).not_to include(I18n.t("errors.messages.too_long", count: limit_for_slug))
-    end
+    it { should_not validate_length_of(:slug).is_at_most(limit_for_slug) }
   end
 end
