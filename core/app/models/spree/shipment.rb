fae264d5db95d6a57f6bebff976d7e5afaa26b05
Nimish <nimish@vinsol.com>
2016-11-08 20:18:00 +0530

    Simplify preloading in shipment and shipping category

--- core/app/models/spree/shipment.rb
+++ core/app/models/spree/shipment.rb
@@ -163,16 +163,18 @@ module Spree
     ManifestItem = Struct.new(:line_item, :variant, :quantity, :states)
 
     def manifest
-      # Grouping by the ID means that we don't have to call out to the association accessor
-      # This makes the grouping by faster because it results in less SQL cache hits.
-      inventory_units.includes(:variant, :line_item).group_by(&:variant_id).map do |variant_id, units|
-        units.group_by(&:line_item_id).map do |line_item_id, units|
+      # Preload data before building manifests
+      grouped_inventory_units = inventory_units.group_by(&:variant_id)
+      variants_hash   = Spree::Variant.unscoped.where(id: grouped_inventory_units.keys).index_by(&:id)
+      line_items_hash = line_items.index_by(&:id)
 
+
+      grouped_inventory_units.map do |variant_id, units|
+        variant = variants_hash[variant_id]
+        units.group_by(&:line_item_id).map do |line_item_id, units|
           states = {}
           units.group_by(&:state).each { |state, iu| states[state] = iu.size }
-
-          line_item = units.first.line_item
-          variant = units.first.variant
+          line_item = line_items_hash[line_item_id]
           ManifestItem.new(line_item, variant, units.length, states)
         end
       end.flatten
